d66e749be429a4a609327eae65310419724cb65b
Donny Wong <donnywong@cs.toronto.edu>
2025-06-26 16:52:25 -0400

    test

--- src/markus_exam_matcher/__main__.py
+++ src/markus_exam_matcher/__main__.py
@@ -29,6 +29,7 @@ def config_arg_parser() -> ArgumentParser:
     parser.add_argument("image", type=str, help="Path to image to predict characters from.")
     parser.add_argument(
         "--char_type",
+        default="digit",
         choices=["digit", "letter"],
         help="Type of character to classify. Only digits and letters are supported.",
     )
