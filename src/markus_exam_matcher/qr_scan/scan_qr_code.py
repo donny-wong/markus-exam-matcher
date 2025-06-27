87c4a06de6dd3f2907dcef0726130c72b5dc0b59
Donny Wong <donnywong@cs.toronto.edu>
2025-06-27 10:09:37 -0400

    test

--- src/markus_exam_matcher/qr_scan/scan_qr_code.py
+++ src/markus_exam_matcher/qr_scan/scan_qr_code.py
@@ -7,6 +7,8 @@ import zxingcpp
 def scan_qr_code(img_path: str) -> str:
     img = cv2.imread(img_path)
     results = zxingcpp.read_barcodes(img)
+    sys.exit(1)
+
     if len(results) == 0:
         print("Could not find any barcode.")
         sys.exit(1)
